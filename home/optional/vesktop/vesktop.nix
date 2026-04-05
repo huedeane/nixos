7bbfec41d00ddfc82c13918c0ce3c40ddf753026
Huedeane <huedeane@gmail.com>
2026-04-05 01:06:09 -0400

    Finish Vesktop

--- home/optional/vesktop/vesktop.nix
+++ home/optional/vesktop/vesktop.nix
@@ -1,14 +1,19 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   programs.vesktop = {
     enable = true;
 
     vencord = {
-
+      extraQuickCss = builtins.readFile ./settings/quickCss.css;
       themes = {
-        catppuccin = ./themes/system24-catppuccin-frappe.theme.css;
+        catppuccin = builtins.readFile ./themes/system24-catppuccin-frappe.theme.css;
       };
     };
   };
+
+  home.activation.vencordSettings = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    ln -sf "$HOME/.config/nixos/home/optional/vesktop/settings/settings.json" \
+    "$HOME/.config/vesktop/settings/settings.json"
+  '';
 }
