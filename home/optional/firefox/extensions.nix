5081325aacb53484f41eb318f1c7520ddbbea11e
Huedeane <huedeane@gmail.com>
2026-04-05 23:54:53 -0400

    Add private browsing access

--- home/optional/firefox/extensions.nix
+++ home/optional/firefox/extensions.nix
@@ -2,16 +2,17 @@
 
 {
   ExtensionSettings = with builtins; let
-    extension = shortId: uuid: {
+    extension = shortId: uuid: privateAccess: {
       name = uuid;
       value = {
         install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
         installation_mode = "force_installed";
+        private_browsing = privateAccess;
       };
     };
   in
     listToAttrs [
-      (extension "sidebery"                   "{3c078156-979c-498b-8990-85f7987dd929}")
-      (extension "firefox-color"              "FirefoxColor@mozilla.com")
+      (extension "sidebery"                   "{3c078156-979c-498b-8990-85f7987dd929}"  true)
+      (extension "firefox-color"              "FirefoxColor@mozilla.com"                false)
     ];
 }
