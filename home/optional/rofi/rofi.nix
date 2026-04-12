f60a6b32111a52ad4def669f2428167f84eb88d4
Huedeane <huedeane@gmail.com>
2026-04-11 23:45:33 -0400

    Add rofi theme

--- home/optional/rofi/rofi.nix
+++ home/optional/rofi/rofi.nix
@@ -3,6 +3,13 @@
 {
   programs.rofi = {
     enable = true;
+    terminal = "kitty";
+    location = "center";
+    theme = ./catppuccin-frappe.rasi;
 
+    extraConfig = {
+      modi = "drun,run,window";
+      show-icons = true;
+    };
   };
 }
