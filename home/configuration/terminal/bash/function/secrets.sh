3421604d5af023f6ee9fc504c896f884ebe6854d
Huedeane <huedeane@gmail.com>
2026-05-25 13:50:09 -0400

    Update git config

--- home/configuration/terminal/bash/function/secrets.sh
+++ home/configuration/terminal/bash/function/secrets.sh
@@ -5,7 +5,7 @@ export GITHUB_USERNAME=$(cat /run/user/1000/secrets/github_username)
 export GITHUB_EMAIL=$(cat /run/user/1000/secrets/github_email)
 export GITHUB_KEY=$(cat /run/user/1000/secrets/github_key)
 
-git config --global credential.helper store
 git config --global user.name "$GITHUB_USERNAME"
 git config --global user.email "$GITHUB_EMAIL"
+git update-index --assume-unchanged ~/.config/nixos/home/configuration/terminal/git/config
 echo "https://$GITHUB_USERNAME:$GITHUB_KEY@github.com" >"$HOME/.git-credentials"
