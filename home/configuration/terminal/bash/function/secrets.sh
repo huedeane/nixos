552a17a1a2464e606fb337cf9d42100fd51b9453
Huedeane <huedeane@gmail.com>
2026-05-28 20:21:25 -0400

    Fix git

--- home/configuration/terminal/bash/function/secrets.sh
+++ home/configuration/terminal/bash/function/secrets.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env sh
 
 export OPENAI_KEY=$(cat /run/user/1000/secrets/chatgpt_key)
-export GITHUB_USERNAME=$(cat /run/user/1000/secrets/github_username)
-export GITHUB_EMAIL=$(cat /run/user/1000/secrets/github_email)
-export GITHUB_KEY=$(cat /run/user/1000/secrets/github_key)
-
-git config --global user.name "$GITHUB_USERNAME"
-git config --global user.email "$GITHUB_EMAIL"
-git update-index --assume-unchanged ~/.config/nixos/home/configuration/terminal/git/config
-echo "https://$GITHUB_USERNAME:$GITHUB_KEY@github.com" >"$HOME/.git-credentials"
--- home/configuration/terminal/git/git.nix
+++ home/configuration/terminal/git/git.nix
@@ -1,7 +1,7 @@
-{ config, editMode, ... }:
-let
-  dirPath = "${config.home.homeDirectory}/.config/nixos/home/configuration/terminal/git";
-  setting = {
+{ config, lib, ... }:
+
+{
+  programs.git = {
     enable = true;
     ignores = [
       ".direnv"
@@ -10,16 +10,41 @@ let
       ".DS_Store"
     ];
     signing.format = null;
+    settings = {
+      init.defaultBranch = "main";
+      pull.rebase = true;
+      push = {
+        autoSetupRemote = true;
+        default = "current";
+      };
+      fetch = {
+        prune = true;
+        prunetags = true;
+      };
+      rerere.enabled = true;
+      diff.algorithm = "histogram";
+      credential.helper = "store";
+      core.autocrlf = "input";
+      submodule.recurse = true;
+    };
+    includes = [
+      { path = "${config.xdg.configHome}/git/identity"; }
+    ];
   };
-in
-{
-  programs.git = setting // (if !editMode then { 
-    includes = [{ path = ./config; }];
-  } else {});
 
- xdg.configFile = if editMode then {
-    "git/config".source = config.lib.file.mkOutOfStoreSymlink "${dirPath}/config";
-  } else {};
+  home.activation.gitCredentials = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    GITHUB_USERNAME=$(cat /run/user/1000/secrets/github_username)
+    GITHUB_EMAIL=$(cat /run/user/1000/secrets/github_email)
+    GITHUB_KEY=$(cat /run/user/1000/secrets/github_key)
+
+    mkdir -p "${config.xdg.configHome}/git"
+    cat > "${config.xdg.configHome}/git/identity" << EOF
+    [user]
+        name = $GITHUB_USERNAME
+        email = $GITHUB_EMAIL
+    EOF
+
+    echo "https://$GITHUB_USERNAME:$GITHUB_KEY@github.com" > "$HOME/.git-credentials"
+  '';
 }
-
 
