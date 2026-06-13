dc9e8ae73420ab88b20ea9532394722e7401b008
Huedeane <huedeane@gmail.com>
2026-06-13 18:51:34 -0400

    Modify tmux

--- home/configuration/terminal/tmux/tmux.nix
+++ home/configuration/terminal/tmux/tmux.nix
@@ -1,8 +1,40 @@
-{ ... }:
-
+{ pkgs, lib, config, editMode, ... }:
+let
+  dirPath = "${config.home.homeDirectory}/.config/nixos/home/configuration/terminal/tmux";
+in
 {
   programs.tmux = {
     enable = true;
 
+    plugins = with pkgs.tmuxPlugins; [
+      yank
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavor 'frappe'
+          set -g @catppuccin_window_status_style 'rounded'
+        '';
+      }
+      resurrect
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '30'
+        '';
+      }
+      prefix-highlight
+      tmux-fzf
+    ]
+    ++ lib.optional editMode {
+      plugin = tmux-autoreload;
+      extraConfig = "set -g @tmux-autoreload-configs '${dirPath}/tmux.conf'";
+    };
+
+    extraConfig =
+      if editMode then ''
+        source-file ${dirPath}/tmux.conf
+      '' else
+        builtins.readFile ./tmux.conf;
   };
 }
