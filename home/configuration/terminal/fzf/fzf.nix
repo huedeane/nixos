55257766bc2d9af892bb281457242e007fe4eee2
Huedeane <huedeane@gmail.com>
2026-06-15 23:24:39 -0400

    Update fzf look

--- home/configuration/terminal/fzf/fzf.nix
+++ home/configuration/terminal/fzf/fzf.nix
@@ -1,14 +1,22 @@
-{ ... }:
-
+{
+  config,
+  editMode,
+  ...
+}:
+let
+  dirPath = "${config.home.homeDirectory}/.config/nixos/home/configuration/terminal/fzf";
+in
 {
   programs.fzf = {
     enable = true;
-    defaultOptions = [
-      "--layout=reverse"
-      "--height=80%"
-      "--border=rounded"
-      "--info=inline"
-      "--preview-window=right:60%:wrap"
-    ];
+    enableBashIntegration = true;
   };
+
+  home.sessionVariables.FZF_DEFAULT_OPTS_FILE = "${config.xdg.configHome}/fzf/fzfrc";
+
+  xdg.configFile."fzf/fzfrc".source =
+    if editMode then
+      config.lib.file.mkOutOfStoreSymlink "${dirPath}/fzfrc"
+    else
+      ./fzfrc;
 }
