0aea64654aa74ad567b4c50dc56895ee5ca58e34
Huedeane <huedeane@gmail.com>
2026-07-28 16:54:43 -0400

    Add edit mode to rmpc

--- home/configuration/terminal/rmpc/rmpc.nix
+++ home/configuration/terminal/rmpc/rmpc.nix
@@ -1,20 +1,54 @@
-{ pkgs, ... }:
-
+{
+  pkgs,
+  lib,
+  editMode,
+  config,
+  ...
+}:
+let
+  dirPath = "${config.home.homeDirectory}/.config/nixos/home/configuration/terminal/rmpc";
+  setting = {
+    enable = true;
+  };
+in
 {
   home.packages = with pkgs; [
-    cava 
+    cava
   ];
 
-  programs.rmpc = {
-    enable = true;
+  programs.rmpc =
+    setting
+    // (
+      if !editMode then
+        {
+          config = builtins.readFile ./config.ron;
+        }
+      else
+        { }
+    );
 
-    config = builtins.readFile ./config.ron;
-  };
-
-  xdg.configFile = {
-    "rmpc/themes".source = ./themes;
-    "rmpc/script".source = ./script;
-  };
+  xdg.configFile =
+    if editMode then
+      lib.mapAttrs' (
+        name: _:
+        lib.nameValuePair "rmpc/themes/${name}" {
+          source = config.lib.file.mkOutOfStoreSymlink "${dirPath}/themes/${name}";
+        }
+      ) (lib.filterAttrs (_: type: type == "regular") (builtins.readDir ./themes))
+      // lib.mapAttrs' (
+        name: _:
+        lib.nameValuePair "rmpc/script/${name}" {
+          source = config.lib.file.mkOutOfStoreSymlink "${dirPath}/script/${name}";
+        }
+      ) (lib.filterAttrs (_: type: type == "regular") (builtins.readDir ./script))
+      // {
+        "rmpc/config.ron".source = config.lib.file.mkOutOfStoreSymlink "${dirPath}/config.ron";
+      }
+    else
+      {
+        "rmpc/themes".source = ./themes;
+        "rmpc/script".source = ./script;
+      };
 
   xdg.desktopEntries."rmpc" = {
     name = "Rmpc";
